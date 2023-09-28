cd6a7b6f0d2607686a8a40a165ba069484c3f022
samuellvn <138622449+samuellvn@users.noreply.github.com>
2023-09-28 15:12:40 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-var randomNumber;
+var randomNumber=randomNumberf();
 
-console.log();
+function randomNumberf=Math.random(a, b) {
+    return Math.floor(Math.random()*(a - b + 1)) + a
+}
+
+console.log(randomNumber);
 
 
 function name(){}
     
-}quickDrawDataset();
+quickDrawDataset();
 array1=["pen","paper","book","bottle"];
 randomNo = Math.floor((Math.random() *array1.length)+1);
 Element_of_array = array1[randomNo];
